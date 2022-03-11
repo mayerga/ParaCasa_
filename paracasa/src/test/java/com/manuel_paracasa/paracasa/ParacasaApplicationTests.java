4eb50da2a8e996fc435d1809d17cd4942baeb4f7
Manuel Yerbes <mayerga4@gmail.com>
2022-03-11 18:18:07 +0100

    Comienzo del proyecto

---
+++ paracasa/src/test/java/com/manuel_paracasa/paracasa/ParacasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manuel_paracasa.paracasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParacasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
