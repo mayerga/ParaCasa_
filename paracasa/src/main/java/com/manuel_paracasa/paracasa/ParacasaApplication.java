61a9618771eaf93db59ed402eccd89cdcf7e961e
Manuel Yerbes <mayerga4@gmail.com>
2022-03-14 17:02:06 +0100

    Comienzo del proyecto y Hello World

---
+++ paracasa/src/main/java/com/manuel_paracasa/paracasa/ParacasaApplication.java
@@ -0,0 +1,13 @@
+package com.manuel_paracasa.paracasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParacasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParacasaApplication.class, args);
+	}
+
+}
